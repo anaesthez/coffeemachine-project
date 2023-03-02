885864df0fa649bef386aa4a070e6c5bfc4479a2
даниил <daniil.ermilov2003de@gmail.com>
2023-03-02 13:59:46 +0500

    1

--- app/src/main/java/ru/ennesterov/coffeemachine/MainActivity.kt
+++ app/src/main/java/ru/ennesterov/coffeemachine/MainActivity.kt
@@ -1,11 +1,50 @@
-package ru.ennesterov.coffeemachine
+import ru.ennesterov.coffeemachine.CoffeeMain
+import ru.ennesterov.coffeemachine.MoscowClass
+import ru.ennesterov.coffeemachine.NewYork
+import kotlin.system.exitProcess
+fun main() {
+    val moscowCoffeeShop = MoscowClass(3.0,4.0,0.5)
+    val newYorkCoffeeShop = NewYork(3.0,4.0,0.5)
+    while (true) {
+        println("Enter city:\n 1.Moscow \n 2. NewYork \n" +
+                " 0. Exist ")
+        val city = readLine()?.toInt()
+        val currentCoffeeShop = when (city) {
+            1 -> {
+                moscowCoffeeShop
+            }
+            2 -> {
+                newYorkCoffeeShop
+            }
+            0 -> {
+                break
+            }
+            else -> {
+                println("Error")
+                exitProcess(1)
+            }
+        }
+        println(
+            "Enter coffee:\n 1.Cappuccino \n 2. Americano \n" +
+                    " 3. Espresso"
+        )
+        val coffee = readLine()?.toInt()
+        val currentCoffee = when(coffee) {
+            1 -> {
+                currentCoffeeShop.makeCappuccino()
+            }
+            2 -> {
+                currentCoffeeShop.makeAmericano()
+            }
+            3 -> {
+                currentCoffeeShop.makeEspresso()
+            }
+            else -> {
+                println("Error")
+                exitProcess(1)
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            }
+        }
+        println("your price is: ${currentCoffeeShop.price()}")
     }
 }
---
+++ app/src/main/java/ru/ennesterov/coffeemachine/MoscowClass.kt
@@ -0,0 +1,22 @@
+package ru.ennesterov.coffeemachine
+
+class MoscowClass(costCappuccino: Double,  costAmericano: Double, costEspresso: Double):
+    CoffeeMain(costCappuccino,costAmericano,costEspresso){
+    var countCappuccino = 0.0
+    var countAmericano = 0.0
+    var countEspresso = 0.0
+    override fun makeCappuccino() {
+        countCappuccino++
+        println("make Cappuccino")
+    }
+    override fun makeAmericano() {
+        countAmericano++
+        println("make Americano")
+    }
+    override fun makeEspresso() {
+        countEspresso++
+        println("make Espresso")
+    }
+    override fun price() = countCappuccino * costCappuccino + countAmericano * costAmericano +
+            + countEspresso * costEspresso
+    }
